6b12c9825582c282d4373605ba87d6b231036453
mufaddalnaya <mufaddal.naya@gmail.com>
2020-09-23 15:08:24 +0530

    Updates

--- search/views.py
+++ search/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from . import WordProcess, QueryProcessing
 # Create your views here.
 
@@ -10,6 +10,9 @@ def home(request):
     return render(request, 'search/home.html')
 
 def search(request):
+
+    if 'keyword' not in request.GET:
+        return HttpResponseRedirect("/")
 
     search_query = request.GET['keyword']
     processed_query = processedDoc.processString(search_query)
