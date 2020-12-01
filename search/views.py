f8d1bbd0f28b86be7830bd13bdd05152d86f7569
mufaddalnaya <mufaddal.naya@gmail.com>
2020-12-01 11:57:58 +0530

    Update

--- search/views.py
+++ search/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from . import WordProcess, QueryProcessing
 # Create your views here.
+# from azlyrics import artists, songs, artists
+
 
 processedDoc = WordProcess.WordProcess()
 queryProcessing = QueryProcessing.QueryProcessing(processedDoc.processed_doc)
+# print(songs("Aaliyah"))
 
 def home(request):
     return render(request, 'search/home.html')
